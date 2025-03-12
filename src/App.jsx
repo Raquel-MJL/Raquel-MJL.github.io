cb758e87b8d14be6cb7a5f31df023d40ddc74a7a
Raquel <148754904+Raquel-MJL@users.noreply.github.com>
2025-03-12 16:29:04 +0100

    EstilosTipografia

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold text-center mb-8">Conjuros de D&D</h1>
+        <h1 className="webTitle text-3xl font-bold text-center mb-8">Conjuros de D&D</h1>
         
         <div className="section-container">
           {Hechizos.map((sectionData) => (
